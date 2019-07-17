3012402cc4cda0d85426735cec9fd2039c31ae15
hugo655 <gayosohugo@gmail.com>
2019-07-17 17:11:22 -0300

    Fix tremolo files

--- src/tremolo.v
+++ src/tremolo.v
@@ -1,4 +1,5 @@
 module tremolo(
+	tri_wave,
 	y,
 	indicator,
 	x,
@@ -9,40 +10,61 @@ module tremolo(
 );
 
 	parameter DATA_WIDTH=32;
-	parameter TRI_WAVE_DATA_WIDTH = 8;
-	parameter TRI_WAVE_UPPER_LIMIT = 30;
+	parameter TRI_WAVE_DATA_WIDTH = 3;
+	parameter TRI_WAVE_UPPER_LIMIT = 3;
 	
-	output reg [DATA_WIDTH-1:0] y;
-	output reg indicator;
+	output wire [DATA_WIDTH-1:0] y;
+	output wire indicator;
 	
 	input 	[DATA_WIDTH-1:0] x;
+	reg 	[DATA_WIDTH-1:0] audio_in;
 	input	en, CLK, audio_ready,rst;
 
-	wire CLK_100;
-	wire [TRI_WAVE_DATA_WIDTH-1:0] tri_wave;
+	wire CLK_2;
+	output wire signed [TRI_WAVE_DATA_WIDTH-1:0] tri_wave;
+	reg signed [DATA_WIDTH+TRI_WAVE_DATA_WIDTH-1:0] prod_result;
 	
-	// Creating a 100 Hz clock
-	clock_divider #(.DIVIDER(500000)) clk_100(.clk_in(CLK),
-														.clk_out(CLK_100),
+	// Creating a 2 Hz clock
+	clock_divider #(.DIVIDER(1250000)) clk_2(.clk_in(CLK),
+														.clk_out(CLK_2),
+														.indicator(indicator),
 														.rst(rst));
 
-	triangular_wave#(.UPPER_LIMIT(TRI_WAVE_UPPER_LIMIT)) triangular_wave0(.tri_wave(tri_wave),
-																		 .CLK(CLK_100),
+	triangular_wave#(.UPPER_LIMIT(TRI_WAVE_UPPER_LIMIT), .DATA_WIDTH(TRI_WAVE_DATA_WIDTH)) triangular_wave0(.tri_wave(tri_wave),
+																		 .CLK(CLK_2),
 																		 .rst(rst));
-																		 
+	
+	
+	tremolo_table#(.DATA_WIDTH(DATA_WIDTH))	trem_table(	.y(y),
+										.counter(tri_wave),
+										.en(en),
+										.x(audio_in));
+										
+//	always @(posedge CLK, negedge rst)
+//	begin
+//		if(~rst)
+//			indicator <= 'b0;
+//		else
+//		begin
+//		if(en)
+//		begin
+//			if(tri_wave == TRI_WAVE_UPPER_LIMIT)
+//				indicator <= 'b1;
+//			else if(tri_wave == 'b0)
+//				indicator <= 'b0;
+//		else	
+//			indicator <= 'b0;
+//		end
+//	end
+//	end
+	
 	always@(posedge CLK, negedge rst)
 	begin
 		if(~rst)
-		begin
-		y <= 'b0;
-		end
+			audio_in <= 'b0;
 		else
-		begin
-			if(tri_wave == TRI_WAVE_UPPER_LIMIT)
-				indicator <= 'b1;
-			else if(tri_wave == 'b0)
-				indicator <= 'b0;
-		end
+			if(audio_ready)
+			audio_in <= x;
 	end
 
 endmodule
